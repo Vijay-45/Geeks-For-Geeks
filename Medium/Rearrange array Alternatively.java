f57e9f00fcfcc7f321a87d2e3d9658bc58bfcc5b
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-02-10 13:17:42 +0530

    Create Rearrange array Alternatively.java

---
+++ Medium/Rearrange array Alternatively.java
@@ -0,0 +1,37 @@
+
+//Time Complexity - O(n)
+//Auxilary Space - O(n)
+
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int[] arr={1,2,3,4,5,6};
+	    Solve(arr);
+	}
+	static void Solve(int[] arr){
+	   int n=arr.length;
+	   int[] temp=new int[n];
+	   int s=0,e=n-1;
+	   int index=0;
+	   while(s<=e){
+	       if(s==e){
+	           temp[index]=arr[s];
+	           break;
+	       }
+	       temp[index++]=arr[e];
+	       temp[index++]=arr[s];
+	       s++;e--;
+	   }
+	   
+	   for(int i=0;i<n;i++){
+	       System.out.print(temp[i]+" ");
+	   }
+	}
+}
+
+_______________________________________________________________________________________________________________________________________________________________________
+
+
+
