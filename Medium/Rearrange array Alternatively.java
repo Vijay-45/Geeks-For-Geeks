f26c71adc5be43f708276d9140c1a8266bae245c
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-02-12 07:18:01 +0530

    Update Rearrange array Alternatively.java

--- Medium/Rearrange array Alternatively.java
+++ Medium/Rearrange array Alternatively.java
@@ -30,6 +30,42 @@ public class Main
 	   }
 	}
 }
+___________________________________________________________________________________________________________________________________________________________________
+
+
+//Time Complexity - O(n)
+//Auxilary Spaace - O(1)
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int[] arr={1,2,3,4,5,6};
+	    Solve(arr);
+	}
+	static void Solve(int[] arr){
+	    int n=arr.length;
+	    int max=arr[n-1]+1;
+	    int min_ind=0,max_ind=n-1;
+	    for(int i=0;i<n;i++){
+	        if(i%2==0){
+	            arr[i]=(arr[max_ind--]%max)*max+arr[i];
+	        }
+	        else if(i%2!=0){
+	            arr[i] = (arr[min_ind++]%max)*max+arr[i];
+	        }
+	    }
+	    
+	    for(int j=0;j<n;j++){
+	        arr[j]=arr[j]/max;
+	    }
+	    
+	    for(int k:arr){
+	        System.out.print(k+" ");
+	    }
+	}
+}
+
 
 
 
