21bf7b346d0cc426568a7b9bd836ddc48a656c05
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-02-17 10:55:21 +0530

    Update Maximum Circular Subarray Sum.java

--- Hard/Maximum Circular Subarray Sum.java
+++ Hard/Maximum Circular Subarray Sum.java
@@ -25,3 +25,48 @@ public class Main
 	  System.out.println(res);
 	}
 }
+_________________________________________________________________________________________________________________________________________________________________________
+
+
+//Time Complexity - O(n)
+//Auxilary Space - O(1)
+
+
+class Solution{
+
+    // a: input array
+    // n: size of array
+    //Function to find maximum circular subarray sum.
+    static int circularSubarraySum(int a[], int n) {
+        
+        // Your code here
+        int Max=Kadane(a,n);
+        if(Max<0){
+            return Max;
+        }
+        
+        return Integer.max(Kadane(a,n), reverseKadane(a,n));
+    }
+    static int Kadane(int[] arr,int n){
+        int curr=arr[0];
+        int res=arr[0];
+        for(int i=1;i<n;i++){
+            curr=Math.max(curr+arr[i],arr[i]);
+            res=Math.max(res,curr);
+        }
+        return res;
+    }
+    static int reverseKadane(int[] arr,int n){
+        int arr_sum=0;
+        for(int i=0;i<n;i++){
+            arr_sum+=arr[i];
+            arr[i]=-arr[i];
+        }
+        int Min_Sum=arr_sum+Kadane(arr,n);
+        
+        return Min_Sum;
+    }
+    
+}
+
+
