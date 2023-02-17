940d310c16d0f88a4493b9140896c4212829b684
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-02-17 07:08:04 +0530

    Update Kadane's Algorithm.java

--- Medium/Kadane's Algorithm.java
+++ Medium/Kadane's Algorithm.java
@@ -22,3 +22,50 @@ class Solution{
     }
     
 }
+
+
+________________________________________________________________________________________________________________________________________________________________________
+
+//Printing the Elements of the maximum subarray of the kadanes's algoritham.
+
+//Time complexity - O(n)
+//Auxilary Space  - O(1)
+
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		int[] arr={-2,-5,6,-2,-3,1,5,-6};
+		Solve(arr);
+	}
+	static void Solve(int[] arr){
+	    int n=arr.length;
+	    int end=0;
+	    int curr=arr[0],global=arr[0];
+	    
+	    for(int i=1;i<n;i++){
+	        curr=Math.max(curr+arr[i],arr[i]);
+	        if(curr>global){
+	            global=curr;
+	            end=i;
+	        }
+	    }
+	    
+	    int start=end;
+	    
+	    
+	    while(start>=0){
+	        global-=arr[start];
+	        if(global==0){
+	            break;
+	        }
+	        start--;
+	    }
+	    
+	    for(int j=start;j<=end;j++){
+	        System.out.print(arr[j]+" ");
+	    }
+	}
+}
+
