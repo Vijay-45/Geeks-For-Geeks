87bff31b8ff2bd94e9a270b7d062d1ccd7a16c88
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-02-20 08:57:17 +0530

    Create Minimum Consecutive Flips.java

---
+++ Easy/Minimum Consecutive Flips.java
@@ -0,0 +1,29 @@
+
+
+//Time Complexity - O(n)
+//Auxilary Space - O(1)
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int[] arr={1,1,0,0,0,1};
+	    Solve(arr);
+	}
+	static void Solve(int[] arr){
+	    int n=arr.length;
+	    for(int i=1;i<n;i++){
+	        if(arr[i]!=arr[i-1]){
+	            if(arr[i]!=arr[0]){
+	                System.out.print("From "+i+" to ");
+	            }
+	            else{
+	                System.out.println(i-1);
+	            }
+	        }
+	    }
+	    if(arr[n-1]!=arr[0]){
+	        System.out.println(n-1);
+	    }
+	}
+}
