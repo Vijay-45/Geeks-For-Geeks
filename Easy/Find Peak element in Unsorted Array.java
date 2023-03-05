eaf58c0cf8c37f3513bafc139a5e2b7f7f33a7b1
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-03-05 07:24:07 +0530

    Update Find Peak element in Unsorted Array.java

--- Easy/Find Peak element in Unsorted Array.java
+++ Easy/Find Peak element in Unsorted Array.java
@@ -29,3 +29,34 @@ public class Main
 	    return 0;
 	}
 }
+
+
+______________________________________________________________________________________________________________________________
+
+
+//Time Complexity - O(n)
+//Auxilary Space - O(1)
+
+
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		int[] arr={80,70,90};
+		System.out.println(Solve(arr));
+	}
+	static int Solve(int[] arr){
+	    int n=arr.length;
+	    if(n==1) return arr[0];
+	    if(arr[0]>arr[1]) return arr[0];
+	    if(arr[n-1]>arr[n-2]) return arr[n-1];
+	    for(int i=1;i<n-1;i++){
+	        if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+	            return arr[i];
+	        }
+	    }
+	    return -1;
+	    
+	}
+}
