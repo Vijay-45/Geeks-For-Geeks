7cbca570e135c95a6c65d58c78125353e6d04569
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-03-21 08:11:49 +0530

    Create BubbleSort.java

---
+++ Easy/BubbleSort.java
@@ -0,0 +1,24 @@
+
+
+//Time Complexity - O(n^2)
+//Auxialry Space - O(1)
+
+
+class Solution
+{
+    //Function to sort the array using bubble sort algorithm.
+	public static void bubbleSort(int arr[], int n)
+    {
+        //code here
+        int temp=0;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    temp=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+    }
+}
