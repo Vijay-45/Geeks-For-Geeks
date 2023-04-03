f7a7cde6542b766120337531de80d421620ab2fa
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-04-03 08:59:10 +0530

    Update Three Way Partition of an Array.java

--- Easy/Three Way Partition of an Array.java
+++ Easy/Three Way Partition of an Array.java
@@ -36,3 +36,48 @@ class Solution{
         }
     }
 }
+
+______________________________________________________________________________________________________________________________
+
+
+
+//Time Complexity - O(n)
+//Auxilary Space - O(1)
+
+
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		int[] arr = {2,4,1,3,5};
+		int a=2;
+		int b=4;
+		Solve(arr,a,b);
+	}
+	static void Solve(int[] arr,int a,int b){
+	    int n=arr.length;
+	    int l=-1,h=n;
+	    int temp=0;
+	    for(int i=0;i<n;i++){
+	        if(arr[i]<a){
+	            l++;
+	            temp=arr[i];
+	            arr[i]=arr[l];
+	            arr[l]=temp;
+	        }
+	    }
+	    for(int i=n-1;i>=0;i--){
+	        if(arr[i]>b){
+	            h--;
+	            temp=arr[i];
+	            arr[i]=arr[h];
+	            arr[h]=temp;
+	        }
+	    }
+	    
+	    for(int i:arr){
+	        System.out.print(i+" ");
+	    }
+	}
+}
