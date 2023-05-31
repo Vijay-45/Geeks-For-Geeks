af97fed8e8579686604eacfe678b58c671651090
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-05-31 13:49:01 +0530

    Update Longest common span with same sum in binary array.java

--- Medium/Longest common span with same sum in binary array.java
+++ Medium/Longest common span with same sum in binary array.java
@@ -31,3 +31,47 @@ public class Main
 	    return res;
 	} 
 }
+
+
+
+_____________________________________________________________________________________________________________________________________________________________________________
+
+
+
+//Time Complexity - O(n)
+//Auxilary Space - O(n)
+
+
+
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		int[] arr={0,1,0,0,0,0};
+		int[] arr1={1,0,1,0,0,1};
+		System.out.println(Solve(arr,arr1));
+	}
+	static int Solve(int[] arr,int[] arr1){
+	    int n=arr.length;
+	    int[] temp = new int[n];
+	    int res=0,preSum=0;
+	    HashMap<Integer,Integer> map =new HashMap<>();
+	    for(int i=0;i<n;i++){
+	        temp[i]=arr[i]-arr1[i];
+	    }
+	    for(int i=0;i<n;i++){
+	        preSum+=temp[i];
+	        if(preSum==0) res=i+1;
+	        if(map.containsKey(preSum)){
+	            res=Math.max(res,i-map.get(preSum));
+	        }
+	        else{
+	            map.put(preSum,i);
+	        }
+	    }
+	    return res;
+	    
+	} 
+}
+
