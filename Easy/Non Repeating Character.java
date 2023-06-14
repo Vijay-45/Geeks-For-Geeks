b222bcafd1d6110f439a1f4ddbd53fc324338a74
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-06-14 07:55:30 +0530

    Update Non Repeating Character.java

--- Easy/Non Repeating Character.java
+++ Easy/Non Repeating Character.java
@@ -60,3 +60,44 @@ public class Main
 	}  
 }
 
+
+____________________________________________________________________________________________________________________________________________________________________________
+
+
+//Time Complexity - O(n)
+//Auxilary Space - O(1)
+
+
+
+
+
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		String s="geeksforgeeks";
+		System.out.println(Solve(s));
+	}
+	static int Solve(String s){
+	    int n=s.length();
+	    int[] arr=new int[256];
+	    Arrays.fill(arr,-1);
+	    for(int i=0;i<n;i++){
+	        if(arr[s.charAt(i)]==-1){
+	            arr[s.charAt(i)]=i;
+	        }
+	        else{
+	            arr[s.charAt(i)]=-2;
+	        }
+	    }
+	    int res=Integer.MAX_VALUE;
+	    for(int i=0;i<256;i++){
+	        if(arr[i]>=0){
+	            res=Math.min(res,arr[i]);
+	        }
+	    }
+	    return res;
+	}
+}
+
