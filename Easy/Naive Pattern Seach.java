51c32de64e3e3620b4ecf4a33ae06136c8b1772b
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-06-17 08:58:39 +0530

    Update Naive Pattern Seach.java

--- Easy/Naive Pattern Seach.java
+++ Easy/Naive Pattern Seach.java
@@ -24,3 +24,80 @@ class Solution
     }
     
 }
+
+_____________________________________________________________________________________________________________________________________________________________________________
+
+
+
+//Time Complexity - O((n-m+1)*m)
+//Auxilary Space - O(1)
+
+
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    String str="AABBCCAACCAABB";
+	    String pat="AABB";
+	    Solve(str,pat);
+	}
+	static void Solve(String str,String pat){
+	    int n=str.length();
+	    int m=pat.length();
+	    for(int i=0;i<n;i++){
+	        int j;
+	        for(j=0;j<m;j++){
+	            if(pat.charAt(j)!=str.charAt(i+j)){
+	                break;
+	            }
+	        }
+	        if(j==m){
+	            System.out.print(i+" ");
+	        }
+	    }
+	} 
+}
+
+
+_____________________________________________________________________________________________________________________________________________________________________________
+
+
+
+//Improved Version of the above code it is considered when there are distinct patterns in the string it works on the Theeta(n)
+
+//Time Complexiy - O(n)
+//Auxilary Space - O(1)
+
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    String str="ABCEABCFABCD";
+	    String pat="ABCD";
+	    Solve(str,pat);
+	}
+	static void Solve(String str,String pat){
+	    int n=str.length();
+	    int m=pat.length();
+	    for(int i=0;i<n;){
+	        int j;
+	        for(j=0;j<m;j++){
+	            if(pat.charAt(j)!=str.charAt(i+j)){
+	                break;
+	            }
+	        }
+	        if(j==m){
+	            System.out.print(i+" ");
+	        }
+	        if(j==0){
+	            i++;
+	        }
+	        else{
+	            i=(i+j);
+	        }
+	    }
+	} 
+}
+
