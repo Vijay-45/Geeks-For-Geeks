40de5472cff830786457cb14b0ecc10adbffd9c7
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-06-22 12:15:05 +0530

    Update Anagram Search.java

--- Easy/Anagram Search.java
+++ Easy/Anagram Search.java
@@ -37,3 +37,51 @@ public class Main
 	    return true;
 	}
 }
+
+
+_____________________________________________________________________________________________________________________________________________________________________________
+
+
+
+//Time Complexity - O(n)
+//Auxilary Space - O(1)
+
+
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		String s="geeksforgeeks";
+		String s1="rseek";
+		System.out.println(Solve(s,s1));
+	}
+	public static final int c=256;
+	static boolean Solve(String s,String s1){
+	    int n=s.length();
+	    int m=s1.length();
+	    int[] arr=new int[c];
+	    int[] arr1=new int[c];
+	    for(int i=0;i<m;i++){
+	        arr[s.charAt(i)]++;
+	        arr1[s1.charAt(i)]++;
+	    }
+	    for(int i=m;i<=(n-m);i++){
+	        if(Same(arr,arr1)){
+	            return true;
+	        }
+	        arr[s.charAt(i)]++;
+	        arr[s.charAt(i-m)]--;
+	    }
+	    return false;
+	}
+	static boolean Same(int[] arr,int[] arr1){
+	    for(int i=0;i<c;i++){
+	        if(arr[i]!=arr1[i]){
+	            return false;
+	        }
+	    }
+	    return true;
+	}
+	
+}
