61f95d03d07ca31874c71d311a8e26a5e8c792e3
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-06-25 12:08:41 +0530

    Update Longest substring with distinct Characters.java

--- Medium/Longest substring with distinct Characters.java
+++ Medium/Longest substring with distinct Characters.java
@@ -72,3 +72,35 @@ public class Main
 	    return res;
 	}
 }
+
+
+______________________________________________________________________________________________________________________________________________________________________________
+
+
+//Time Complexity - O(n)
+//Auxilary Space - O(1)
+
+
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		String s="abac";
+		System.out.println(Solve(s));
+	}
+	static int Solve(String s){
+	   int n=s.length();
+	   int maxEnd=0;
+	   int i=0,res=0;
+	   int[] arr=new int[256];
+	   Arrays.fill(arr,-1);
+	   for(int j=0;j<n;j++){
+	       i=Math.max(i,arr[s.charAt(j)]+1);
+	       maxEnd=j-i+1;
+	       res=Math.max(res,maxEnd);
+	       arr[s.charAt(j)]=j;
+	   }
+	   return res;
+	}
+}
