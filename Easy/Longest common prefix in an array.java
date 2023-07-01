0154eb96832072632819accb1768857f51319512
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-07-01 12:01:03 +0530

    Update Longest common prefix in an array.java

--- Easy/Longest common prefix in an array.java
+++ Easy/Longest common prefix in an array.java
@@ -33,3 +33,30 @@ class Solution{
         return "-1";
     }
 }
+
+
+
+______________________________________________________________________________________________________________________________________________________________________________
+
+
+
+//Time Complexity - O(n*m*logn)
+//Auxilary Space  - O(m)
+
+
+class Solution {
+    public String longestCommonPrefix(String[] arr) {
+        int n=arr.length;
+        int i=0,j=0;
+        String res="";
+        Arrays.sort(arr);
+        while(i<arr[0].length() && j<arr[n-1].length()){
+            if(arr[0].charAt(i)!=arr[n-1].charAt(j)){
+                break;
+            }
+            res+=arr[0].charAt(i);
+            i++;j++;
+        }
+        return res;
+    }
+}
