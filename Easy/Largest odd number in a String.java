61f1c4e8254d74d19110b28e310d0e93d6a4752a
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-07-02 07:54:16 +0530

    Update Largest odd number in a String.java

--- Easy/Largest odd number in a String.java
+++ Easy/Largest odd number in a String.java
@@ -11,12 +11,11 @@ class Solution {
     String maxOdd(String s) {
         // code here
         int n=s.length();
-        String res="";
-        for(int i=0;i<n;i++){
+        for(int i=n-1;i>=0;i--){
             if((s.charAt(i)-'0')%2!=0){
-                res=s.substring(0,i+1);
+                return s.substring(0,i+1);
             }
         }
-        return res;
+        return "";
     }
 }
