9099c997bff4d25eae865c27cb0c970356d16f7f
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-08-10 11:36:12 +0530

    Update Add two numbers represented by linked list.java

--- Medium/Add two numbers represented by linked list.java
+++ Medium/Add two numbers represented by linked list.java
@@ -5,6 +5,13 @@
 
 
 
+/* In this problem we need to add two lists it includes three steps as: 
+    -> First reverse the two input lists as the sizes of the lists can be different.
+    -> After reversing both the lists add the data of the lists 
+    -> After adding reverse the retruned list as it is the result list.  */
+
+
+
 class Solution{
     //Function to add two numbers represented by linked list.
     static Node addTwoLists(Node first, Node second){
@@ -44,9 +51,13 @@ class Solution{
         Node tail=null;
         while(first!=null || second!=null || carry!=0){
             int val1=0;
+            // Condition for checking if first becomes null it can be handled here if it dont it will cause NullPointerException
             if(first!=null) val1=first.data;
+            
             int val2=0;
+            //Same as First One
             if(second!=null) val2=second.data;
+            
             int sum=val1+val2+carry;
             int dig=sum%10;
             Node temp=new Node(dig);
