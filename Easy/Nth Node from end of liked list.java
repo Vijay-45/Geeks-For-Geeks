38bf0c5ad17d55dfdc1070807c4593c7bf07a102
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-08-16 14:06:11 +0530

    Update Nth Node from end of liked list.java

--- Easy/Nth Node from end of liked list.java
+++ Easy/Nth Node from end of liked list.java
@@ -29,3 +29,32 @@ class Solution
     	return -1;
     }
 }
+
+
+____________________________________________________________________________________________________________________________
+
+
+//Time Complexity - O(n)
+//Auxilary Space - o(1)
+
+
+
+class Solution
+{
+    //Function to find the data of nth node from the end of a linked list.
+    int getNthFromLast(Node head, int n)
+    {
+        Node first=head;
+        Node second=head;
+        for(int i=0;i<n;i++){
+            if(first==null) return -1;
+            first=first.next;
+        }
+        while(first!=null){
+            first=first.next;
+            second=second.next;
+        }
+        return second.data;
+    }
+}
+
