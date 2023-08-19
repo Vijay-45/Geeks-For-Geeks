4b82dc67633e3f1cc01651ef580ee4445f58683a
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-08-19 16:55:57 +0530

    Update Reverse a linked list in groups of size k.java

--- Medium/Reverse a linked list in groups of size k.java
+++ Medium/Reverse a linked list in groups of size k.java
@@ -25,3 +25,46 @@ static Node Solve(Node head,int k){
 	    }
 	    return prev;
 	}
+
+
+
+_____________________________________________________________________________________________________________________________
+
+
+
+
+
+//Time Complexity - O(n)
+//Auxilary Space - O(1)
+
+
+
+
+static Node Solve(Node head,int k){
+	   Node curr=head,temp=null,prevFirst=null;
+	   boolean flag=true;
+	   while(curr!=null){
+	       Node first=curr,prev=null;
+	       int count=0;
+	       while(curr!=null && count<k){
+	           temp=curr.next;
+	           curr.next=prev;
+	           prev=curr;
+	           curr=temp;
+	           count++;
+	       }
+		//To store the head node after first window reversal
+	       if(flag){
+	           head=prev;
+	           flag=false;
+	       }
+	       else{
+		//Connecting last node of the previous windoes with the first node of the current node
+	           prevFirst.next=prev;
+	       }
+		//Storing the first node from every window
+	       prevFirst=first;
+	       
+	   }
+	   return head;
+	}
