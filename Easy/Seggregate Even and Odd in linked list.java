11ff19816032564a2acebe93ce94282326221292
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-08-24 11:05:05 +0530

    Update Seggregate Even and Odd in linked list.java

--- Easy/Seggregate Even and Odd in linked list.java
+++ Easy/Seggregate Even and Odd in linked list.java
@@ -35,3 +35,53 @@
 	    temp.next=null;
 	    return head;
 	}
+
+____________________________________________________________________________________________________________________________
+
+
+
+
+//Time Complexity - O(n)
+//Auxilary Space - O(1)
+
+//One Traversal
+
+static Node EvenAndOdd(Node head){
+	    Node curr=head;
+	    Node OddS=null,OddE=null,EvenS=null,EvenE=null;
+	    while(curr!=null){
+	        if(curr.data%2!=0){
+	//If OddS is null make two pointers point to the same Node 
+	            if(OddS==null){
+	                OddS=curr;
+	                OddE=curr;
+	            }
+	//If Not then move OddE to the next Node and make them Connected
+	            else{
+	                OddE.next=curr;
+	                OddE=OddE.next;
+	                
+	            }
+	        }
+	//Same Process as we did for the Odd Nodes
+	        else{
+	            if(EvenS==null){
+	                EvenS=curr;
+	                EvenE=curr;
+	            }
+	            else{
+	                EvenE.next=curr;
+	                EvenE=EvenE.next;
+	            }
+	        }
+	        curr=curr.next;
+	    }
+	//If List Only Even Or Odd nodes this condition will handle that case
+	    if(EvenS==null || OddS==null){
+	        return head;
+	    }
+	    EvenE.next=OddS;
+	    OddE.next=null;
+	    return EvenS;
+	    
+	}
